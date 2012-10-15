cc803ed7e732d5c5255526cef0b091b559717865
ilink <ian@linkportfolio.net>
2012-10-14 19:12:15 -0700

    loading coordinates from the browers

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,28 @@
 $(document).ready(function(){
-	
-	$.ajax({
-		type: 'GET',
-		url: '/weather.json',
-		success: function(data){
-			var weathercolor = new Weathercolor();
-			var layout = new Layout({
-				h: data.temp,
-				s: 1,
-				v: data.temp
-			}, $('.wrapper'));
-			layout.each(function($item, col, row, i){
-				$item.css({
-					'background-color': weathercolor.make(i, row, col, data)
+
+	get_coords(function(geo){
+		console.log(geo);
+
+		var coords = geo.coords.latitude + "," + geo.coords.longitude;
+		var url = "/weather.json?coords="+coords;
+
+		$.ajax({
+			type: 'GET',
+			url: url,
+			success: function(data){
+				var weathercolor = new Weathercolor();
+				var layout = new Layout({
+					h: data.temp,
+					s: 1,
+					v: data.temp
+				}, $('.wrapper'));
+				layout.each(function($item, col, row, i){
+					$item.css({
+						'background-color': weathercolor.make(i, row, col, data)
+					});
 				});
-			});
-		}
+			}
+		});
 	});
 	
 });
--- public/js/util.js
+++ public/js/util.js
@@ -12,3 +12,9 @@ map from range1 to range2
 function fit_bound(x, min1, max1, min2, max2){
 	return (max2 - min2) * (x-min1) / (max1 - min1) + min2;
 }
+
+function get_coords(callback){
+	navigator.geolocation.getCurrentPosition(function(geo){
+		callback.call(this, geo);
+	});
+}
