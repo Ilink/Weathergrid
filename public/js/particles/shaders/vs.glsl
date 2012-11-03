37e6228adbb7e364eccaaeb87b77b158d95ccba6
ilink <ian@linkportfolio.net>
2012-11-03 13:19:32 -0700

    switched extension name to glsl

---
+++ public/js/particles/shaders/vs.glsl
@@ -0,0 +1,12 @@
+<script id="vs" type="x-shader/vertex"> 
+ 
+	attribute vec3 position;
+
+	uniform mat4 uMVMatrix;
+	uniform mat4 uPMatrix;
+
+	void main(void) {
+	    gl_Position = uPMatrix * uMVMatrix * vec4(position, 1.0);
+	}
+
+</script>
