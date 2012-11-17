fc7fe54725f2581bbd0ee54da7bf14e2158a79a8
ilink <ian@linkportfolio.net>
2012-11-16 16:29:54 -0800

    stuff

--- public/js/particles/compositor.js
+++ public/js/particles/compositor.js
@@ -3,8 +3,16 @@ Attach a compositor to a piece of geometry?
 set and unset a bunch of framebuffers
 */
 
-function Compositor(){
-	this.composite = function(){
+function Compositor(gl){
 
-	}
+    // Create a framebuffer
+    var fbo = gl.createFramebuffer();
+    framebuffers.push(fbo);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+
+    this.composite = function(texture){
+        // Attach a texture to it.
+        gl.framebufferTexture2D(
+            gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
+    }
 }
