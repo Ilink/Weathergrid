e75cff7c878e257e6bcd317e470d2d2ee0d61312
ilink <ian@linkportfolio.net>
2012-11-18 00:40:14 -0800

    rtt working kind of

--- public/js/particles/blur_compositor.js
+++ public/js/particles/blur_compositor.js
@@ -24,13 +24,37 @@ function BlurCompositor(gl, shaderPair){
     var program = shaderProgramBuilder.build(shaderPair);
     self.programs.push(program);
 
+    var size = $('canvas').width();
+
+    // Create a color texture
+    var colorTexture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, colorTexture);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, size, size, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+
+    // Create the depth buffer
+    var depthBuffer = gl.createRenderbuffer();
+    gl.bindRenderbuffer(gl.RENDERBUFFER, depthBuffer);
+    gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, size, size);
+
+    // Create the framebuffer
+    var framebuffer = gl.createFramebuffer();
+
+
     /*
     Ping pongs and does whatever
     then returns a texture result, or the final framebuffer
     */
     this.compose = function(setup, geo, texture){
         var textureResult;
-        gl.bindFramebuffer(gl.FRAMEBUFFER, fbos[0]);
+        gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
+        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, colorTexture, 0);
+        gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, depthBuffer);
+
+        // gl.bindFramebuffer(gl.FRAMEBUFFER, fbos[0]);
         gl.useProgram(program);
         setup(program);
 
